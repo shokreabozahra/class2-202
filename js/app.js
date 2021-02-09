f4d4092c562104845534a733ef20b91adff60621
Sahar Zahran <saharzahran666@yahoo.com>
2021-02-09 06:23:26 +0200

    add some files

---
+++ js/app.js
@@ -0,0 +1,9 @@
+alert("Hi");
+
+let userName = prompt("What is your name?");
+
+alert("Hi " + userName);
+
+document.write("welcome in my website!");
+
+alert("It was a great time " + userName + ", come to visit our website again!");
